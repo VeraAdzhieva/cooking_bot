d03160770b93ad2c05296b357bcf6c998094f38d
Vera Adzhieva <vera.adzhieva3003@mail.ru>
2026-08-28 12:45:47 +0300

    update bot

---
+++ agents/graph.py
@@ -0,0 +1,79 @@
+import os
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, MessagesState, END
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from agents.nodes import create_router_node, create_recipe_node, create_planner_node, reject_node
+from utils.logger import setup_logger
+
+logger = setup_logger()
+graph_app = None
+
+class CustomState(MessagesState):
+    next_nodes: list[str]
+
+async def setup_graph():
+    global graph_app
+
+    #async_http_client = httpx.AsyncClient(
+    #    proxy="socks5://127.0.0.1:10808",
+    #    trust_env=False
+    #)
+
+    logger.info("Инициализация LLM...")
+    llm = ChatOpenAI(
+        api_key=os.getenv("LLM_API_KEY"),
+        base_url=os.getenv("LLM_HOST"),
+        model=os.getenv("LLM_MODEL"),
+        temperature=0.0,
+      #  http_async_client=async_http_client
+    )
+    
+    # Загружаем ВСЕ инструменты из MCP
+    logger.info("Загрузка инструментов MCP...")
+    mcp_config = {
+        "my_local_mcp": {
+            "transport": "http",
+            "url": os.getenv("MCP_HOST"), 
+            "headers": {"Authorization": f"Bearer {os.getenv('MCP_API_KEY')}"}
+        }
+    }
+    
+    client = MultiServerMCPClient(mcp_config)
+    all_tools = await client.get_tools()
+
+    logger.info("Создание узлов графа (все инструменты доступны всем агентам)")
+    
+    graph = StateGraph(CustomState)
+    
+    graph.add_node("router", create_router_node(llm))
+    graph.add_node("recipes", create_recipe_node(llm, all_tools))
+    graph.add_node("planner", create_planner_node(llm, all_tools))
+    graph.add_node("reject", reject_node)
+    
+    graph.set_entry_point("router")
+    
+    def route_decision(state: CustomState):
+        nodes_to_visit = state.get("next_nodes", ["reject"])
+        if isinstance(nodes_to_visit, str):
+            nodes_to_visit = [nodes_to_visit]
+            
+        logger.info(f"Роутер направляет запрос в узлы: {nodes_to_visit}")
+        return nodes_to_visit
+        
+    graph.add_conditional_edges("router", route_decision, {
+        "recipes": "recipes",
+        "planner": "planner",
+        "reject": "reject"
+    })
+    
+    graph.add_edge("recipes", END)
+    graph.add_edge("planner", END)
+    graph.add_edge("reject", END)
+    
+    graph_app = graph.compile()
+    logger.info("LangGraph успешно скомпилирован!")
+
+def get_graph():
+    if graph_app is None:
+        raise RuntimeError("Граф не инициализирован. Вызовите setup_graph() первым.")
+    return graph_app
